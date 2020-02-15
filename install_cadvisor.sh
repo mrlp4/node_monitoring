9cbaada7de0c5fdeac4b663540696541b285c809
Kiryl <laptanovi4@gmail.com>
2020-02-15 18:02:01 +0300

    scripts upd

---
+++ install_cadvisor.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+# Run cadvisor in docker on specified port:
+
+sudo docker run   --volume=/:/rootfs:ro   --volume=/var/run:/var/run:ro   --volume=/sys:/sys:ro   --volume=/var/lib/docker/:/var/lib/docker:ro   --volume=/dev/disk/:/dev/disk:ro   --publish=9080:8080   --detach=true   --name=cadvisor   gcr.io/google-containers/cadvisor:latest
+
